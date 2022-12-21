5191a143c3594425b33b4cfd043e3717b350d78a
Alena Shanskaya <terkuna@gmail.com>
2022-12-21 18:30:56 +0300

    update homework3

--- src/test/java/ru/levelp/at/levelp/at/homework3/TaskThreeMailTest.java
+++ src/test/java/ru/levelp/at/levelp/at/homework3/TaskThreeMailTest.java
@@ -1,7 +1,6 @@
 package ru.levelp.at.levelp.at.homework3;
 
 import com.google.common.base.Verify;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -16,13 +15,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class TaskThreeDeleteSendMailTest extends LocatorsBaseTest{
+public class TaskThreeMailTest extends LocatorsBaseTest{
     String subject = "My beautiful theme";
-
-    @BeforeEach
-    void setUp() {
-       super.setUp();
-    }
 
     @Test
     void testDeleteSendMail() {
@@ -31,67 +25,60 @@ public class TaskThreeDeleteSendMailTest extends LocatorsBaseTest{
 
         driver.navigate().to("https://mail.ru/");
 
-        //---------------------1. Войти в почту
+//---------------------1. Войти в почту
 
-        driver.findElement(By.cssSelector("[data-testid='enter-mail-primary']")).click(); //нажатие на КНОПКУ "ВОЙТИ"
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-testid='enter-mail-primary']"))).click(); //нажатие на КНОПКУ "ВОЙТИ"
 
-        WebElement frame = driver.findElement(By.cssSelector("iframe.ag-popup__frame__layout__iframe")); //нашли фрейм, в котором открылась авторизация
-        WebDriver frameDriver = driver.switchTo().frame(frame); //переключились на него
-        frameDriver.findElement(By.cssSelector("[name='username']")).sendKeys(USER_LOGIN); //вводим адресс почты уже во фрейме
+        WebElement frame = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("iframe.ag-popup__frame__layout__iframe"))); //нашли фрейм, в котором открылась авторизация
+        driver.switchTo().frame(frame); //переключились на него
 
-        frameDriver.findElement(By.cssSelector("[name='username']")).sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
-        // frameDriver.findElement(By.cssSelector("[data-test-id='next-button']")).click(); //клик на КНОПКУ "Ввести пароль"
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='username']"))).sendKeys(USER_LOGIN); //вводим адресс почты уже во фрейме
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='username']"))).sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
 
-        frameDriver.findElement(By.cssSelector("[name='password']")).sendKeys(USER_PASSWORD); //вводим пароль
-
-        frameDriver.findElement(By.cssSelector("[name='password']")).sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
-        // frameDriver.findElement(By.cssSelector("[data-test-id='submit-button']")).click(); //клик на КНОПКУ "ВОЙТИ"
-
-        //---------------------2.Assert, что вход выполнен успешно
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='password']"))).sendKeys(USER_PASSWORD); //вводим пароль
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='password']"))).sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
 
         driver.navigate().refresh();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.ph-project__account")));
+
+//---------------------2.Assert, что вход выполнен успешно
+
         assertThat(USER_LOGIN).isEqualTo(driver.findElement(By.cssSelector("div.ph-project__account")).getAttribute("aria-label")); //сравниваю адреса почты
 
 //--------------------- 2.1.Заходим в Входящие и проверяем колличество писем ДО отправки нового
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ph-project-promo-close-icon"))).click();//закрыть всплывашку
 
-        driver.findElement(By.cssSelector(BUTTON_TOMYSELF)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(BUTTON_TOMYSELF))).click();
         wait.until(ExpectedConditions.urlToBe(URL_PAGE_TOMYSELF));
 
         int incomingLetterCountBefore = driver.findElements(By.cssSelector("a.js-letter-list-item")).size();
 
 //-------------------- 2.2. Заходим в папку Корзина и проверяем колличество писем ДО удаления нового
 
-        driver.findElement(By.cssSelector(BUTTON_TRASH)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(BUTTON_TRASH))).click();
         wait.until(ExpectedConditions.urlToBe(URL_PAGE_TRASH));
 
         int deleteLetterCountBefore = driver.findElements(By.cssSelector("a.js-letter-list-item")).size();
 
 //---------------------3. Создать новое письмо (заполнить адресата (самого себя), тему письма и тело)
 
-        driver.findElement(By.cssSelector(BUTTON_COMPOSE)).click(); //нажали на кнопку "Написать письмо"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(BUTTON_COMPOSE))).click();
 
-        driver.findElement(By.cssSelector(".container--H9L5q")).click();
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys(to);
-
-        driver.findElement(By.cssSelector("[name='Subject']")).click();
-        driver.findElement(By.cssSelector("[name='Subject']")).sendKeys(subject);
-
-        driver.findElement(By.cssSelector("div.cke_editable")).sendKeys(bodyText);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='text']"))).sendKeys(to);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='Subject']"))).sendKeys(subject);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.cke_editable"))).sendKeys(bodyText);
 
 //---------------------4.Отправить письмо
 
-        driver.findElement(By.cssSelector("[data-test-id='send']")).click(); //нажатие на КНОПКУ "Отправить"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test-id='send']"))).click(); //нажатие на КНОПКУ "Отправить"
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class*='button2_close']")));
-        driver.findElement(By.cssSelector("span[class*='button2_close']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class*='button2_close']"))).click();
 
 //---------------------5. Verify, что письмо появилось в папке Входящие
 
-        driver.findElement(By.cssSelector(BUTTON_TOMYSELF)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(BUTTON_TOMYSELF))).click();
         wait.until(ExpectedConditions.urlToBe(URL_PAGE_TOMYSELF));
-
         driver.navigate().refresh();
 
         List<WebElement> incomingLetter = driver.findElements(By.cssSelector("a.js-letter-list-item"));
@@ -106,24 +93,23 @@ public class TaskThreeDeleteSendMailTest extends LocatorsBaseTest{
         driver.navigate().to(hrefLastLetter);
         driver.navigate().refresh();
 
-        String emailAddresLeter = driver.findElement(By.cssSelector("span.letter-contact")).getAttribute("title");
+        String emailAddresLeter = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".letter__recipients > .letter-contact"))).getAttribute("title");
         Verify.verify(emailAddresLeter.equals(to));
 
-        String subjectMail = driver.findElement(By.cssSelector("h2.thread-subject")).getText();
+        String subjectMail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h2.thread-subject"))).getText();
         Verify.verify(subjectMail.equals(subject));
 
-        String emailValue = driver.findElement(By.cssSelector("div.js-readmsg-msg > div > div > div > div")).getText();
+        String emailValue = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.js-readmsg-msg > div > div > div > div"))).getText();
         Verify.verify(emailValue.equals(bodyText));
 
 //---------------------7. Удалить письмо
 
-        driver.findElement(By.cssSelector("span[class*='button2_delete']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class*='button2_delete']"))).click();
 
 //---------------------8. Verify что письмо появилось в папке Корзина
 
-        driver.findElement(By.cssSelector(BUTTON_TRASH)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(BUTTON_TRASH))).click();
         wait.until(ExpectedConditions.urlToBe(URL_PAGE_TRASH));
-
         driver.navigate().refresh();
 
         List<WebElement> deleteLetter = driver.findElements(By.cssSelector("a.js-letter-list-item"));
@@ -132,8 +118,8 @@ public class TaskThreeDeleteSendMailTest extends LocatorsBaseTest{
 
 //---------------------9. Выйти из учётной записи
 
-        driver.findElement(By.cssSelector("span[class*='ph-dropdown-icon']")).click();
-        driver.findElement(By.cssSelector("div[data-testid*='whiteline-account-exit']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class*='ph-dropdown-icon']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-testid='whiteline-account-exit']"))).click();
        }
 }
 
