f1ca9f493e024d2942ffa57b0235fcbf4b2a36ee
Alena Shanskaya <terkuna@gmail.com>
2022-12-26 20:44:00 +0300

    update homework2

--- src/test/java/ru/levelp/at/levelp/at/homework2/LotteryTicketNegativeTest.java
+++ src/test/java/ru/levelp/at/levelp/at/homework2/LotteryTicketNegativeTest.java
@@ -1,19 +1,19 @@
 package ru.levelp.at.levelp.at.homework2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.levelp.at.homework2.LotteryTicket;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class LotteryTicketNegativeTest {
 
     @Tag("Negative")
     @ParameterizedTest
-    @ValueSource(ints = {000, 1111,})
+    @ValueSource(ints = {1111})
 
-    void billetIsBad(int input){
+    void billetIsBad(int input) {
         boolean expected = false;
         boolean actual = LotteryTicket.billet(input);
         assertThat(actual).isEqualTo(expected);
--- src/test/java/ru/levelp/at/levelp/at/homework2/LotteryTicketPositiveTest.java
+++ src/test/java/ru/levelp/at/levelp/at/homework2/LotteryTicketPositiveTest.java
@@ -1,11 +1,11 @@
 package ru.levelp.at.levelp.at.homework2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.levelp.at.homework2.LotteryTicket;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class LotteryTicketPositiveTest {
 
@@ -13,7 +13,7 @@ public class LotteryTicketPositiveTest {
     @ParameterizedTest
     @ValueSource(ints = {111111, 222222})
 
-    void sumGood(int input){
+    void sumGood(int input) {
         boolean expected = true;
         boolean actual = LotteryTicket.billet(input);
         assertThat(actual).isEqualTo(expected);
@@ -23,7 +23,7 @@ public class LotteryTicketPositiveTest {
     @ParameterizedTest
     @ValueSource(ints = {11111, 22222, })
 
-    void billetIsBad(int input){
+    void billetIsBad(int input) {
         boolean expected = false;
         boolean actual = LotteryTicket.billet(input);
         assertThat(actual).isEqualTo(expected);
@@ -33,10 +33,9 @@ public class LotteryTicketPositiveTest {
     @ParameterizedTest
     @ValueSource(ints = {111112, 222223})
 
-    void sumIsNotGood(int input){
+    void sumIsNotGood(int input) {
         boolean expected = false;
         boolean actual = LotteryTicket.billet(input);
         assertThat(actual).isEqualTo(expected);
     }
-
 }
