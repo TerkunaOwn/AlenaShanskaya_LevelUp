47224e955d6c4fb8c661f26d2daa92135407c009
Alena Shanskaya <terkuna@gmail.com>
2022-12-26 20:37:30 +0300

    update homework3

--- src/test/java/ru/levelp/at/levelp/at/homework3/TaskTwoMailTest.java
+++ src/test/java/ru/levelp/at/levelp/at/homework3/TaskTwoMailTest.java
@@ -11,11 +11,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TaskThreeMailTest extends LocatorsBaseTest {
-    String subject = "My beautiful theme";
+public class TaskTwoMailTest extends LocatorsBaseTest {
+    String subject = "My beautiful theme to test the word Test";
 
     @Test
-    void testDeleteSendMail() {
+    void testUsingFilterToSendEmail() {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
 
@@ -30,19 +30,18 @@ public class TaskThreeMailTest extends LocatorsBaseTest {
             By.cssSelector("iframe.ag-popup__frame__layout__iframe"))); //нашли фрейм, в котором открылась авторизация
         driver.switchTo().frame(frame); //переключились на него
 
-        wait.until(ExpectedConditions.elementToBeClickable(
-            By.cssSelector("[name='username']"))).sendKeys(userLogin); //вводим адресс почты уже во фрейме
-        wait.until(ExpectedConditions.elementToBeClickable(
-            By.cssSelector("[name='username']"))).sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='username']")))
+            .sendKeys(userLogin); //вводим адресс почты уже во фрейме
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='username']")))
+            .sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
 
-        wait.until(ExpectedConditions.elementToBeClickable(
-            By.cssSelector("[name='password']"))).sendKeys(userPassword); //вводим пароль
-        wait.until(ExpectedConditions.elementToBeClickable(
-            By.cssSelector("[name='password']"))).sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='password']")))
+            .sendKeys(userPassword); //вводим пароль
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='password']")))
+            .sendKeys(Keys.ENTER); //имитируем нажатие клавиши ENTER
 
         driver.navigate().refresh();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-            By.cssSelector("div.ph-project__account")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.ph-project__account")));
 
         // 2.Assert, что вход выполнен успешно
 
@@ -51,21 +50,21 @@ public class TaskThreeMailTest extends LocatorsBaseTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(
             By.cssSelector(".ph-project-promo-close-icon"))).click(); //закрыть всплывашку
 
-        // 2.1.Заходим в Входящие и проверяем колличество писем ДО отправки нового
+        // 2.1.Заходим в отправленные и проверяем колличество писем ДО создания нового
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonMySelf))).click();
-        wait.until(ExpectedConditions.urlToBe(urlPageToMySelf));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonSent))).click();
+        wait.until(ExpectedConditions.titleIs(pageTitleSent));
 
-        final int incomingLetterCountBefore = driver.findElements(By.cssSelector("a.js-letter-list-item")).size();
+        final int sendLetterCountBefore = driver.findElements(By.cssSelector("a.js-letter-list-item")).size();
 
-        // 2.2. Заходим в папку Корзина и проверяем колличество писем ДО удаления нового
+        // 2.2. Заходим в папку Тест и проверяем колличество писем ДО создания нового
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonTrash))).click();
-        wait.until(ExpectedConditions.urlToBe(urlPageTrash));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonTest))).click();
+        wait.until(ExpectedConditions.urlToBe(urlPageTest));
 
-        final int deleteLetterCountBefore = driver.findElements(By.cssSelector("a.js-letter-list-item")).size();
+        final int packageLetterCountBefore = driver.findElements(By.cssSelector("a.js-letter-list-item")).size();
 
-        // 3. Создать новое письмо (заполнить адресата (самого себя), тему письма и тело)
+        // 3.Создать новое письмо (заполнить адресата (самого себя), тему письма (должно содержать слово Тест) и тело)
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonCompose))).click();
 
@@ -80,19 +79,27 @@ public class TaskThreeMailTest extends LocatorsBaseTest {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(
             By.cssSelector("[data-test-id='send']"))).click(); //нажатие на КНОПКУ "Отправить"
-        wait.until(ExpectedConditions.elementToBeClickable(
-            By.cssSelector("span[class*='button2_close']")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class*='button2_close']")));
         wait.until(ExpectedConditions.visibilityOfElementLocated(
             By.cssSelector("span[class*='button2_close']"))).click();
 
-        // 5. Verify, что письмо появилось в папке Входящие
+        // 5. Verify, что письмо появилось в папке отправленные
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonMySelf))).click();
-        wait.until(ExpectedConditions.urlToBe(urlPageToMySelf));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.cssSelector(buttonSent))).click();
+        wait.until(ExpectedConditions.titleIs(pageTitleSent));
         wait.until(ExpectedConditions.numberOfElementsToBe(
-            By.cssSelector("a.js-letter-list-item"), incomingLetterCountBefore + 1));
+            By.cssSelector("a.js-letter-list-item"), sendLetterCountBefore + 1));
 
-        // 6. Verify контент, адресата и тему письма (должно совпадать с пунктом 3)
+        // 6. Verify, что письмо появилось в папке Тест
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonTest))).click();
+        wait.until(ExpectedConditions.urlToBe(urlPageTest));
+
+        wait.until(ExpectedConditions.numberOfElementsToBe(
+            By.cssSelector("a.js-letter-list-item"), packageLetterCountBefore + 1));
+
+        // 7. Verify контент, адресата и тему письма (должно совпадать с пунктом 3)
 
         wait.until(ExpectedConditions.elementToBeClickable(
             By.cssSelector("a.js-letter-list-item" + ":nth-of-type(1)"))).click();
@@ -109,20 +116,7 @@ public class TaskThreeMailTest extends LocatorsBaseTest {
             By.cssSelector("div.js-readmsg-msg > div > div > div > div"))).getText();
         Verify.verify(emailValue.equals(bodyText));
 
-        // 7. Удалить письмо
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-            By.cssSelector("span[class*='button2_delete']"))).click();
-
-        // 8. Verify что письмо появилось в папке Корзина
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(buttonTrash))).click();
-        wait.until(ExpectedConditions.urlToBe(urlPageTrash));
-
-        wait.until(ExpectedConditions.numberOfElementsToBe(
-            By.cssSelector("a.js-letter-list-item"), deleteLetterCountBefore + 1));
-
-        // 9. Выйти из учётной записи
+        // 8. Выйти из учётной записи
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(
             By.cssSelector("span[class*='ph-dropdown-icon']"))).click();
